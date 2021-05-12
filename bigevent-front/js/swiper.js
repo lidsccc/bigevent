f9519be7f4062838dc60a474bf194a7e1d2ec6ba
daishuang <454932030@qq.com>
2021-05-12 10:12:08 +0800

    完成了comment和link前台模块,后台评论模块

---
+++ bigevent-front/js/swiper.js
@@ -0,0 +1,36 @@
+// 入口函数
+$(function () {
+    // 轮播图列表渲染
+    getswpList()
+    function getswpList() {
+        $.ajax({
+            method: 'GET',
+            url: '/api/swipers',
+            success: function (res) {
+                if (res.status !== 0) {
+                    return res.message
+                }
+                // 模板渲染
+                var htmlStr = template('swp-tpl', res)
+                $('#swp').html(htmlStr)
+                layui.form.render()
+
+                // 轮播图的layui格式
+                layui.use('carousel', function () {
+                    var carousel = layui.carousel;
+                    //建造实例
+                    carousel.render({
+                        elem: '#kr_carousel'
+                        // , width: '100%' //设置容器宽度
+                        , height: '100%'
+                        , arrow: 'always' //始终显示箭头
+                        //,anim: 'updown' //切换动画方式
+                    });
+                });
+            }
+        })
+    }
+    // 轮播图
+
+
+})
